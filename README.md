380f5750d8c0c949e5f9057bae3880ca304cd8fc
Nguyen Khoi <khoibene@gmail.com>
2025-04-02 11:01:03 +0700

    day2

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 "# ExpressJS" 
+
+Bài 2: {
+  -Xây dựng các API liên quan đến User
+  -Code CRUD
+  -req.params
+  -req.body
+}
--- index.js
+++ index.js
@@ -1,37 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import userRoute from './routes/user.route.js'
+
 dotenv.config();  
 
 const PORT = process.env.PORT
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     message: "Hello World",
-//     name: "Khoi",
-//     age: 19
-//   })
-// })
+app.use(express.json())
 
-// app.listen(8000, () => {
-//   console.log(`Sv is running on http://localhost:${PORT}`);
-// })
-
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: "Hello Bitch",
-    name: "Nguyen Huu Khoi",
-    age: 19
-  })
+app.get('/home', (req, res) => {
+  res.send('Home')
 })
 
-app.listen(8000)
+app.use(userRoute)
 
-//Sự khác nhau giữa import và require
-//So sánh giữa Web Server(Nginx, Apache) và Server Backend
-//Tìm hiểu những cái vừa code và folder có trong project  
-//IP là địa chỉ của 1 thiết bị duy nhất trên mạng
-//Port là chuỗi số nguyên xác định một ứng dụng hay dịch vụ trên một thiết bị trong mạng
-//Node JS là 1 runtime environment, giúp chạy JS trên server thay vì chỉ trên trình duyệt, V8 Engine, Hỗ trợ bất đồng bộ, tăng hiệu suất
-//Express là 1 framework giúp code JS dễ dàng hơn 
+app.listen(8000, () => {
+  console.log(`Sv is running on http://localhost:${PORT}`);
+})
+
